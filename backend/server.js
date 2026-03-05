1c666c28c1ec43741b1d880942055d7926324313
dev-train42 <hioxtest42@gmail.com>
2026-03-05 16:57:14 +0530

    server file update

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  process.exit(1);
+  res.send("API Root Working 🚀 v3");
 });
 
 app.get("/users", (req, res) => {
   res.json([
-    { id: 1, name: "Rider 1 - v2" },
-    { id: 2, name: "Pillion 1 - v2" }
+    { id: 1, name: "Rider 1" },
+    { id: 2, name: "Pillion 2" }
   ]);
 });
 
